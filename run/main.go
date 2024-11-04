e68255e4caf7db14ea802fa50b8b35a6c403cc6a
Wangjarimm <karamissuu@gmail.com>
2024-11-04 08:50:25 +0700

    start with port 8081

--- run/main.go
+++ run/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"net/http"
+	"log"
 
 	"github.com/gocroot/route"
 )
 
 func main() {
+	// Memastikan aplikasi terus berjalan dengan server HTTP
 	http.HandleFunc("/", route.URL)
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server berjalan di port 8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatalf("Gagal menjalankan server: %v", err)
+	}
 }
