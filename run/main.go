4d739f6bd316d84b21849322801acbdce52451f8
Wangjarimm <karamissuu@gmail.com>
2024-11-04 18:26:35 +0700

    add func initdb and log if error to connect mongodb

--- run/main.go
+++ run/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 
+	"github.com/gocroot/config"  // Pastikan untuk mengimpor paket config
 	"github.com/gocroot/route"
 )
 
 func main() {
+	// Inisialisasi koneksi database
+	config.InitMongoDB()
+
+	// Mengecek apakah koneksi database berhasil
+	if config.ErrorMongoconn != nil {
+		log.Fatalf("Gagal terhubung ke MongoDB: %v", config.ErrorMongoconn)
+	}
+
 	// Memastikan aplikasi terus berjalan dengan server HTTP
 	http.HandleFunc("/", route.URL)
 	log.Println("Server berjalan di port 8081")
