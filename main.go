ee63995bad4d9b0f3176a38996c1b252af2c2fd4
Wangjarimm <karamissuu@gmail.com>
2024-11-04 08:49:57 +0700

    log if error to connect db

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package gocroot
 
 import (
+	"log"
+	"github.com/gocroot/config"  // Import paket config untuk koneksi database
 	"github.com/gocroot/route"
-
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
 func init() {
+	// Mengecek apakah koneksi database berhasil
+	if config.ErrorMongoconn != nil {
+		log.Fatalf("Gagal terhubung ke MongoDB: %v", config.ErrorMongoconn)
+	}
+
+	// Mendaftarkan fungsi HTTP untuk Google Cloud Functions
 	functions.HTTP("WebHook", route.URL)
 }
+
+
