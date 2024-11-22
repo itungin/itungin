64fbb51e41ee62f231f5d4c9d8645b6c2d56a231
waskithocitoadiwiguno <waskithocito.a@gmail.com>
2024-11-22 12:23:21 +0700

    TRY

--- config/api.go
+++ config/api.go
@@ -25,3 +25,7 @@ var WAAPIToken string
 var APIGETPDLMS string = "https://pamongdesa.kemendagri.go.id/webservice/public/user/get-by-phone?number="
 
 var APITOKENPD string = os.Getenv("PDTOKEN")
+
+var PRIVATEKEY string = "e4cb06d20bcce42bf4ac16c9b056bfaf1c6a5168c24692b38eb46d551777dc4147db091df55d64499fdf2ca85504ac4d320c4c645c9bef75efac0494314cae94"
+
+var PUBLICKEY string = "47db091df55d64499fdf2ca85504ac4d320c4c645c9bef75efac0494314cae94"
