493a5a05787d961d48fb9b10d916893270174583
waskithocitoadiwiguno <waskithocito.a@gmail.com>
2024-12-05 11:46:44 +0700

    3

--- controller/gis.go
+++ controller/gis.go
@@ -7,31 +7,31 @@ import (
 	"github.com/gocroot/config"
 	"github.com/gocroot/helper/at"
 	"github.com/gocroot/helper/atdb"
-	"github.com/gocroot/helper/watoken"
+	// "github.com/gocroot/helper/watoken"
 	"github.com/gocroot/model"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetRegion(respw http.ResponseWriter, req *http.Request) {
-	_, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
-	if err != nil {
-		var respn model.Response
-		respn.Status = "Error : Token Tidak Valid "
-		respn.Info = "public Key :" + config.PublicKeyWhatsAuth
-		respn.Location = "Decode Token Error: " + at.GetLoginFromHeader(req)
-		respn.Response = err.Error()
-		at.WriteJSON(respw, http.StatusForbidden, respn)
-		return
-	}
+	// _, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	// if err != nil {
+	// 	var respn model.Response
+	// 	respn.Status = "Error : Token Tidak Valid "
+	// 	respn.Info = "public Key :" + config.PublicKeyWhatsAuth
+	// 	respn.Location = "Decode Token Error: " + at.GetLoginFromHeader(req)
+	// 	respn.Response = err.Error()
+	// 	at.WriteJSON(respw, http.StatusForbidden, respn)
+	// 	return
+	// }
 	var longlat model.LongLat
-	err = json.NewDecoder(req.Body).Decode(&longlat)
-	if err != nil {
-		var respn model.Response
-		respn.Status = "Error : Body tidak valid"
-		respn.Response = err.Error()
-		at.WriteJSON(respw, http.StatusBadRequest, respn)
-		return
-	}
+	json.NewDecoder(req.Body).Decode(&longlat)
+	// if err != nil {
+	// 	var respn model.Response
+	// 	respn.Status = "Error : Body tidak valid"
+	// 	respn.Response = err.Error()
+	// 	at.WriteJSON(respw, http.StatusBadRequest, respn)
+	// 	return
+	// }
 	filter := bson.M{
 		"border": bson.M{
 			"$geoIntersects": bson.M{
