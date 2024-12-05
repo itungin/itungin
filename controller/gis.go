e878e5d543d51103e9f0e1deaa1736d1a74906e2
waskithocitoadiwiguno <waskithocito.a@gmail.com>
2024-12-05 13:47:42 +0700

    Json

--- controller/gis.go
+++ controller/gis.go
@@ -12,31 +12,100 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func GetRegion(respw http.ResponseWriter, req *http.Request) {
+// func GetRegion(respw http.ResponseWriter, req *http.Request) {
 
+// 	var longlat model.LongLat
+// 	json.NewDecoder(req.Body).Decode(&longlat)
+
+// 	filter := bson.M{
+// 		"border": bson.M{
+// 			"$geoIntersects": bson.M{
+// 				"$geometry": bson.M{ // Menggunakan $geometry, bukan $border
+// 					"type":        "Point",
+// 					"coordinates": []float64{longlat.Longitude, longlat.Latitude},
+// 				},
+// 			},
+// 		},
+// 	}
+	
+	
+// 	region, err := atdb.GetOneDoc[model.Region](config.MongoconnGeo, "region", filter)
+// 	if err != nil {
+// 		at.WriteJSON(respw, http.StatusNotFound, region)
+// 		return
+// 	}
+// 	at.WriteJSON(respw, http.StatusOK, region)
+// }
+
+//anton
+func GetRegion(respw http.ResponseWriter, req *http.Request) {
+	// Dekode token untuk autentikasi
+	// _, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	// if err != nil {
+	// 	var respn model.Response
+	// 	respn.Status = "Error : Token Tidak Valid"
+	// 	respn.Location = "Decode Token Error: " + at.GetLoginFromHeader(req)
+	// 	respn.Response = err.Error()
+	// 	at.WriteJSON(respw, http.StatusForbidden, respn)
+	// 	return
+	// }
+
+	// Parse koordinat dari body request
 	var longlat model.LongLat
 	json.NewDecoder(req.Body).Decode(&longlat)
+	// if err != nil {
+	// 	var respn model.Response
+	// 	respn.Status = "Error : Body tidak valid"
+	// 	respn.Response = err.Error()
+	// 	at.WriteJSON(respw, http.StatusBadRequest, respn)
+	// 	return
+	// }
 
+	// Filter query geospasial
 	filter := bson.M{
 		"border": bson.M{
 			"$geoIntersects": bson.M{
-				"$geometry": bson.M{ // Menggunakan $geometry, bukan $border
+				"$geometry": bson.M{
 					"type":        "Point",
 					"coordinates": []float64{longlat.Longitude, longlat.Latitude},
 				},
 			},
 		},
 	}
-	
-	
+
+	// Cari region berdasarkan filter
 	region, err := atdb.GetOneDoc[model.Region](config.MongoconnGeo, "region", filter)
 	if err != nil {
-		at.WriteJSON(respw, http.StatusNotFound, region)
+		at.WriteJSON(respw, http.StatusNotFound, bson.M{"error": "Region not found"})
 		return
 	}
-	at.WriteJSON(respw, http.StatusOK, region)
+
+	// Format respon sebagai FeatureCollection GeoJSON
+	geoJSON := bson.M{
+		"type": "FeatureCollection",
+		"features": []bson.M{
+			{
+				"type": "Feature",
+				"geometry": bson.M{
+					"type":        region.Border.Type,
+					"coordinates": region.Border.Coordinates,
+				},
+				"properties": bson.M{
+					"province":     region.Province,
+					"district":     region.District,
+					"sub_district": region.SubDistrict,
+					"village":      region.Village,
+				},
+			},
+		},
+	}
+
+	// Kirim respon dalam format GeoJSON
+	at.WriteJSON(respw, http.StatusOK, geoJSON)
 }
 
+
+//GET ROADSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
 // new anton add
 func GetRoads(respw http.ResponseWriter, req *http.Request) {
 
